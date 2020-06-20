99b1a9333119417312407e64af15ef7add5fa377
Sarbjot <sarbjotmann98@gmail.com>
2020-06-20 13:01:41 -0700

    Forced Landscape Orientation, added table layout, table activity needs to be implemented in game file

--- app/src/main/java/ca/sfu/cmpt_276_a3/MinesweeperGame.java
+++ app/src/main/java/ca/sfu/cmpt_276_a3/MinesweeperGame.java
@@ -4,12 +4,11 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+public class MinesweeperGame extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-
+        setContentView(R.layout.activity_minesweeper_game);
     }
 }
