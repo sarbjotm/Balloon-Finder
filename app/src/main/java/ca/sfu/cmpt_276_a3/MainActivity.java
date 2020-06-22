3c86c8c52d51858b63ea8ea853eeb7f9d06b241b
Sarbjot <sarbjotmann98@gmail.com>
2020-06-21 17:14:19 -0700

    Created Welcome animation

--- app/src/main/java/ca/sfu/cmpt_276_a3/MainActivity.java
+++ app/src/main/java/ca/sfu/cmpt_276_a3/MainActivity.java
@@ -6,23 +6,38 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
+
+import com.daimajia.androidanimations.library.Techniques;
+import com.daimajia.androidanimations.library.YoYo;
 
 public class MainActivity extends AppCompatActivity {
+    private TextView welcomeText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         launchButton();
+        animation();
 
     }
 
+    private void animation(){
+        welcomeText = findViewById(R.id.welcome);
+        YoYo.with(Techniques.Wave)
+                .duration(5000)
+                .repeat(0)
+                .playOn(welcomeText);
+    }
+
     private void launchButton(){
-        Button btn = (Button) findViewById(R.id.btnTest);
+        Button btn = (Button) findViewById(R.id.btnSkip);
         btn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-            Intent intent = MinesweeperGame.makeIntent(MainActivity.this);
+            Intent intent = WelcomeScreen.makeIntent(MainActivity.this);
+            finish();
             startActivity(intent);
             }
         });
--- app/src/main/java/ca/sfu/cmpt_276_a3/WelcomeScreen.java
+++ app/src/main/java/ca/sfu/cmpt_276_a3/WelcomeScreen.java
@@ -2,6 +2,8 @@ package ca.sfu.cmpt_276_a3;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
 
 public class WelcomeScreen extends AppCompatActivity {
@@ -10,5 +12,9 @@ public class WelcomeScreen extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_welcome_screen);
+    }
+
+    public static Intent makeIntent(Context context){
+        return new Intent(context,WelcomeScreen.class);
     }
 }
