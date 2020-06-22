712077022c029e861333f3f614ee3f494b42141d
Sarbjot <sarbjotmann98@gmail.com>
2020-06-21 23:04:49 -0700

    - Back Button bug fixed - Theme applied - Icon Changed

--- app/src/main/java/ca/sfu/cmpt_276_a3/MainActivity.java
+++ app/src/main/java/ca/sfu/cmpt_276_a3/MainActivity.java
@@ -64,6 +64,7 @@ public class MainActivity extends AppCompatActivity {
                 changeActivity = 1;
                 Intent intent = WelcomeScreen.makeIntent(MainActivity.this);
                 startActivity(intent);
+                finish();
             }
         });
 
