4e9540bc09221666b3e0d506b8262175d6c8c242
Sarbjot <sarbjotmann98@gmail.com>
2020-06-22 17:49:37 -0700

    Radio Buttons

--- app/src/main/java/ca/sfu/cmpt_276_a3/MinesweeperGame.java
+++ app/src/main/java/ca/sfu/cmpt_276_a3/MinesweeperGame.java
@@ -24,9 +24,11 @@ Helpful tips and tricks from Dr.Brian Fraser Videos
 
  */
 public class MinesweeperGame extends AppCompatActivity {
-    private static final int NUM_ROWS = 5;
-    private static final int NUM_COLS = 10;
+    private static int NUM_ROWS = 4;
+    private static int NUM_COLS = 6;
+    private static int NUM_MINES = 6;
     Button buttons[][] = new Button[NUM_ROWS][NUM_COLS];
+    private int test = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -35,8 +37,22 @@ public class MinesweeperGame extends AppCompatActivity {
         populateTable();
     }
 
+
     @SuppressLint("SetTextI18n")
     private void populateTable(){
+//        int numRows = Integer.parseInt(String.valueOf(Settings.getMatrixSize(this).charAt(0)));
+//        NUM_ROWS = numRows;
+//        if (NUM_ROWS == 4){
+//            NUM_COLS = 6;
+//        }
+//        else if(NUM_ROWS == 5){
+//            NUM_COLS =10;
+//        }
+//        else{
+//            NUM_COLS = 15;
+//        }
+        int numMines = Settings.getMinesAmount(this);
+        NUM_MINES = numMines;
         TableLayout table = (TableLayout) findViewById(R.id.tableGame);
         for(int row = 0; row < NUM_ROWS; row++){
             TableRow tablerow = new TableRow(this);
@@ -71,7 +87,6 @@ public class MinesweeperGame extends AppCompatActivity {
 
     private void gridButtonClicked(int row, int col){
         Button button = buttons[row][col];
-
         //Prevents button being changed after image
         for(int rowAdjust = 0; rowAdjust < NUM_ROWS; rowAdjust++){
             for(int colAdjust = 0; colAdjust < NUM_COLS; colAdjust++){
@@ -89,7 +104,16 @@ public class MinesweeperGame extends AppCompatActivity {
         Bitmap scaledBitmap = Bitmap.createScaledBitmap(originalBitmap, newWidth, newHeight, true);
         Resources resource = getResources();
         button.setBackground(new BitmapDrawable(resource, scaledBitmap));
-            }
+        }
+
+
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+//        populateTable();
+    }
+
     public static Intent makeIntent(Context context){
         return new Intent(context,MinesweeperGame.class);
     }
--- app/src/main/java/ca/sfu/cmpt_276_a3/Settings.java
+++ app/src/main/java/ca/sfu/cmpt_276_a3/Settings.java
@@ -3,9 +3,12 @@ package ca.sfu.cmpt_276_a3;
 import androidx.appcompat.app.AppCompatActivity;
 import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.RadioButton;
 import android.widget.RadioGroup;
+import android.widget.Toast;
 
 public class Settings extends AppCompatActivity {
 
@@ -16,33 +19,86 @@ public class Settings extends AppCompatActivity {
         setContentView(R.layout.activity_settings);
         createRadioButtonsForMatrix();
         createRadiobuttonForMines();
-
+        String savedMatrixValue = getMatrixSize(this);
+        Toast.makeText(this,"Saved: " + savedMatrixValue, Toast.LENGTH_LONG).show();
+        int savedMinesAmount = getMinesAmount(this);
     }
 
     private void createRadioButtonsForMatrix(){
         RadioGroup group = (RadioGroup)findViewById(R.id.radioGroupBoard);
         String[] matrixSize = getResources().getStringArray(R.array.rowCol);
         for(int i = 0; i < matrixSize.length; i++){
-            String matrix_size = matrixSize[i];
+            final String matrix_size = matrixSize[i];
             RadioButton button = new RadioButton(this);
             button.setText(matrix_size);
+            button.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    saveMatrixSize(matrix_size);
+                }
+            });
             group.addView(button);
+
+            //Select default button
+            if (matrix_size == getMatrixSize(this)){
+                button.setChecked(true);
+            }
+
 
         }
     }
-
 
     private void createRadiobuttonForMines(){
         RadioGroup group2 = (RadioGroup)findViewById(R.id.radioGroupMines);
         int[] num_mines = getResources().getIntArray(R.array.num_of_mines);
         for(int j = 0; j < num_mines.length; j++){
-            int num_mine = num_mines[j];
+            final int num_mine = num_mines[j];
             RadioButton button = button = new RadioButton(this);
             button.setText(num_mine +"");
+            button.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    saveNumberMines(num_mine);
+                }
+            });
             group2.addView(button);
+            if (num_mine == getMinesAmount(this)){
+                button.setChecked(true);
+            }
 
         }
     }
+
+    private void saveMatrixSize(String matrix_size) {
+        SharedPreferences prefs = this.getSharedPreferences("AppPrefs",MODE_PRIVATE);
+        SharedPreferences.Editor editor = prefs.edit();
+        editor.putString("MatrixSize", matrix_size);
+        editor.apply();
+    }
+
+
+    private void saveNumberMines(int num_mine) {
+        SharedPreferences prefs = this.getSharedPreferences("AppPrefs2",MODE_PRIVATE);
+        SharedPreferences.Editor editor = prefs.edit();
+        editor.putInt("MineAmount", num_mine);
+        editor.apply();
+    }
+
+    //static so any activity can access
+    static public String getMatrixSize(Context context){
+        SharedPreferences prefs = context.getSharedPreferences("AppPrefs",MODE_PRIVATE);
+        String defaultValue2 = context.getResources().getString(R.string.default_size);
+        return prefs.getString("MatrixSize",defaultValue2);
+
+    }
+
+    static public int getMinesAmount(Context context){
+        SharedPreferences prefs = context.getSharedPreferences("AppPrefs2",MODE_PRIVATE);
+        int defaultValue = context.getResources().getInteger(R.integer.default_mines);
+        return prefs.getInt("MineAmount", defaultValue);
+
+    }
+
 
 
 
